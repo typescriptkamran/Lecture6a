b708eea8b4162f7a34d8315d038407f82eeca77c
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-22 01:24:06 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+/*  
+Steps to star node project with typescript:
+1. install node js 18.17.1 LTS  version from https://nodejs.org/en
+2. to install typesript from command prompt run: 
+install npm install -g typescript
+3. open new folder with vs code editor
+Make a file index.ts
+4. to initialize the project with typescript: 
+tsc --init
+tsc
+5. Make it a Node.js project by giving the following command:
+
+ npm init -y
+
+6. nstall types module for Node.js with command on command prompt:
+ npm i @types/node -D
+
+7. Need to change in tsconfig.json and package.json files according to following documentation:
+https://www.typescriptlang.org/docs/handbook/esm-node.html
+in tsconfig.json:
+line 14:  "target": "ES2022",
+line 28:  "module": "NodeNext",
+line 30:  "moduleResolution": "NodeNext"
+if using json files then uncomment the following line
+line 42:  "resolveJsonModule": true,
+
+in package.json add the following under name:
+in line 3: "type": "module",
+8. make .gitignore file and write in it: 
+ node_modules
+
+ */
